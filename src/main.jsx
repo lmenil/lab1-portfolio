5413d984d80fa9b98f61fb45b11f2e833412dbdf
lmenil <lmenil@my.centennialcollege.ca>
2024-09-20 04:22:30 -0400

    10th update

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home  from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import  Projects  from "./pages/Projects.jsx";
-import  Services  from "./pages/Services.jsx";
-import  Contact  from "./pages/Contact.jsx";
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/vite-react/",
-    element: <App />,
-    children: [
-      {
-        path: "/vite-react/",
-        element: <Home />,
-      },
-      {
-        path: "/vite-react/About",
-        element: <About />,
-      },
-      {
-        path: "/vite-react/Projects",
-        element: <Projects />,
-      },
-      {
-        path: "/vite-react/Services",
-        element: <Services />,
-      },
-      {
-        path: "/vite-react/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
