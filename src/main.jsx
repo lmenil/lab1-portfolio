7ffe12f9fd8d9b5090ca6a2ab4b9629fb6e85a6a
lmenil <lmenil@my.centennialcollege.ca>
2024-09-21 10:37:44 -0400

    fix6

--- src/main.jsx
+++ src/main.jsx
@@ -5,29 +5,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 
-import { createBrowserRouter } from "react-router-dom";
-import { Home } from "./pages/Home.jsx";
-import { About } from "./pages/About.jsx";
-
-const router=createBrowserRouter([
-  {
-    path: "/vite-react/",
-    element: <App />,
-    children: [
-      {
-        path: "/vite-react",
-        element: <Home />,
-      },
-      {
-        path: "/vite-react",
-        element: <About />,
-      },
-    ]
-}])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
 
